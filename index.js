6ed2b52135da2a358140402283ce016f00faafca
Hasan-Rifat <developerhr000@gmail.com>
2023-02-23 09:59:51 +0600

    express and node js setup

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// database connection
+// dbConnect()
+
+// routes
+// products api
+/* app.use("/api/v1/products", productsRouter); */
+
+// users api
+/* app.use("/api/v1/users", usersRouter); */
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).json({
+    status: "fail",
+    message: "Route not found",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
